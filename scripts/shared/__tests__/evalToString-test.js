f8933e07d0844d79119f88e15d1360fb21ff1edb
Himanshi Gupta <89007259+himanshiLt@users.noreply.github.com>
2022-03-30 17:34:45 +0530

    Add new test and suite to development branch

--- scripts/shared/__tests__/evalToString-test.js
+++ scripts/shared/__tests__/evalToString-test.js
@@ -23,6 +23,34 @@ describe('evalToString', () => {
     expect(parseAndEval(`'foo ' + 'bar'`)).toBe('foo bar');
   });
 
+  it('add test to developemnt branch should support string concat (`+`)', () => {
+    expect(parseAndEval(`'foo ' + 'bar'`)).toBe('foo bar');
+  });
+
+  it('should throw when it finds other types', () => {
+    expect(() => parseAndEval(`'foo ' + true`)).toThrowError(
+      /Unsupported type/
+    );
+    expect(() => parseAndEval(`'foo ' + 3`)).toThrowError(/Unsupported type/);
+    expect(() => parseAndEval(`'foo ' + null`)).toThrowError(
+      /Unsupported type/
+    );
+    expect(() => parseAndEval(`'foo ' + undefined`)).toThrowError(
+      /Unsupported type/
+    );
+  });
+});
+
+describe('Add suite to developement branch | evalToString', () => {
+  it('should support StringLiteral', () => {
+    expect(parseAndEval(`'foobar'`)).toBe('foobar');
+    expect(parseAndEval(`'yowassup'`)).toBe('yowassup');
+  });
+
+  it('should support string concat (`+`)', () => {
+    expect(parseAndEval(`'foo ' + 'bar'`)).toBe('foo bar');
+  });
+
   it('should throw when it finds other types', () => {
     expect(() => parseAndEval(`'foo ' + true`)).toThrowError(
       /Unsupported type/
